972cb5c6a8d14542fa3cfc56426b96534ce11c00
Student 243442 <243442@student.pwr.edu.pl>
2020-11-18 20:18:16 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+using System.Windows.Threading;
+using System.IO;
+
+namespace SinP2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SystemInfo systemInfo = new SystemInfo();
+        DispatcherTimer Timer99 = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowBlur.SetIsEnabled(this, true);
+            MouseDown += Window_MouseDown;
+
+            try
+            {
+                InitializeSinP();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("System info data error: " + e.Message);
+            }
+
+            Timer99.Tick += Timer99_Tick; // don't freeze the ui
+            Timer99.Interval = new TimeSpan(0, 0, 0, 0, 1024);
+            Timer99.IsEnabled = true;
+        }
+        private bool InitializeSinP()
+        {
+            setComputerName();    
+            setOSName();
+            setCPUName();
+            setGPUName();
+            setRAMName();
+            setDriveName();
+            return true;
+        }
+        public int getCPUCounter()
+        {
+            PerformanceCounter cpuCounter = new PerformanceCounter();
+            cpuCounter.CategoryName = "Processor";
+            cpuCounter.CounterName = "% Processor Time";
+            cpuCounter.InstanceName = "_Total";
+
+            // will always start at 0
+            dynamic firstValue = cpuCounter.NextValue();
+            System.Threading.Thread.Sleep(1000);
+            // now matches task manager reading
+            dynamic secondValue = cpuCounter.NextValue();
+
+            return (int)secondValue;
+        }
+
+
+        private void Timer99_Tick(Object sender, EventArgs e)
+        {
+            var percent = (int)getCPUCounter();
+            CpuUsage.Content = percent + " % CPU";
+            progress.Value = percent;
+            if (percent > 80)
+                progress.Foreground = new SolidColorBrush(Colors.Red);
+            else
+                progress.Foreground = new SolidColorBrush(Colors.Green);
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MinimizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void setComputerName()
+        {
+            CName.Content = systemInfo.MachineName;
+        }
+        private void setOSName()
+        {
+            OSName.Content = systemInfo.OSName;
+        }
+        private void setCPUName()
+        {
+            ProcessorName.Content = systemInfo.CPUName;
+        }
+        private void setGPUName()
+        {
+            GraphicName.Content = systemInfo.GPUName;
+        }
+        private void setRAMName()
+        {
+            RAMName.Content = systemInfo.RAMName;
+        }
+        private void setDriveName()
+        {
+            DriveName.Content = systemInfo.DriveName;
+        }
+
+    }
+}
+
---
+++ SystemInfo.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.Management;
+using System.Diagnostics;
+
+namespace SinP2
+{
+    class SystemInfo
+    {
+
+        public string MachineName = "";
+        public string OSName = "";
+        public string CPUName = "";
+        public string GPUName = "";
+        public string RAMName = "";
+        public string DriveName = "";
+        public List<string> ProcessorInfo = new List<string>();
+        public List<string> VisualInfo = new List<string>();
+        public List<string> RAMInfo = new List<string>();
+        public List<List<string>> DrivesInfo = new List<List<string>>();
+
+        public SystemInfo()
+        {
+            var os = Environment.OSVersion;
+            OSName = os.VersionString + " Platform " + os.Platform + " V " + os.Version;
+            MachineName = Environment.MachineName;
+            getProcessor();
+            if (ProcessorInfo.Count != 0)
+                CPUName = ProcessorInfo[0];
+            else
+                CPUName = "N/A";
+
+            getVisual();
+            if (VisualInfo.Count != 0)
+                GPUName = VisualInfo[0];
+            else
+                GPUName = "N/A";
+
+            getRAM();
+
+            getLogicalDrivers();
+
+        }
+        private void getProcessor()
+        {
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor");
+            foreach (ManagementObject mo in mos.Get())
+            {
+                ProcessorInfo.Add((string)mo["Name"]);
+            }
+        }
+
+        private void getVisual()
+        {
+            using (var searcher = new ManagementObjectSearcher("select * from Win32_VideoController"))
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    VisualInfo.Add((string)obj["Name"]);
+                }
+            }
+        }
+
+        private void getRAM()
+        {
+            string Query = "SELECT Capacity FROM Win32_PhysicalMemory";
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(Query);
+
+            UInt64 Capacity = 0,
+                temp = 0;
+
+            foreach (ManagementObject WniPART in searcher.Get())
+            {
+                temp = Convert.ToUInt64(WniPART.Properties["Capacity"].Value);
+                temp /= (ulong)Math.Pow(1024, 3);
+                RAMInfo.Add(temp.ToString());
+                Capacity += temp;
+            }
+
+            // TEST
+            //RAMInfo[0] = "5";
+            //RAMInfo[1] = "2";
+            //RAMInfo[2] = "3";
+            //RAMInfo[3] = "2";
+
+            if (RAMInfo.Count != 0)
+            {
+                string checkSameValues = RAMInfo[0];
+                bool identical = true;
+                foreach (var single in RAMInfo)
+                {
+                    if (single != checkSameValues)
+                        identical = false;
+                }
+                if (identical)
+                    RAMName = "Total " + Capacity.ToString() + "GB  | " + RAMInfo.Count.ToString() + " x " + RAMInfo[0].ToString() + "GB |";
+                else
+                {
+                    RAMName = "Total " + Capacity.ToString() + "GB  |";
+
+                    for (var i = 0; i < RAMInfo.Count; ++i)
+                    {
+                        RAMName += RAMInfo[i].ToString() + "GB | ";
+                    }
+                }
+            }
+            else
+                RAMName = "N/A";
+        }
+        private void getLogicalDrivers()
+        {
+            foreach (var drive in System.IO.DriveInfo.GetDrives())
+            {
+                double freeSpace = drive.TotalFreeSpace;
+                double totalSpace = drive.TotalSize;
+                double percentFree = (freeSpace / totalSpace) * 100;
+                float num = (float)percentFree;
+                string percent = percentFree.ToString(),
+                    template = drive.ToString(),
+                    totalSize = (drive.TotalSize / (Math.Pow(1024, 3))).ToString();
+                string[] split = percent.Split(',');
+                percent = split[0] + "% ";
+                split = null;
+                split = totalSize.Split(',');
+                totalSize = split[0] + "GB ";
+
+                List<string> disc = new List<string>();
+
+                disc.Add(template.Remove(1,2) +":");
+                disc.Add(percent);
+                disc.Add(totalSize);
+
+                DrivesInfo.Add(new List<string>(disc));
+            }
+            if (DrivesInfo.Count != 0)
+            {
+                for (var i = 0; i < DrivesInfo.Count; ++i)
+                {
+                    for (var l = 0; l < DrivesInfo[i].Count; ++l)
+                        DriveName += DrivesInfo[i][l] + " ";
+                    DriveName += "| ";
+                }
+                DriveName.Remove(DriveName.Length - 1, 1);
+            }
+            else
+                DriveName = "N/A";
+        }
+
+        public string getCurrentCpuUsage()
+        {
+            PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+
+            return cpuCounter.NextValue() + "%";
+        }
+
+        public string getAvailableRAM()
+        {
+            PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+            return ramCounter.NextValue() + "MB";
+        }
+    }
+}
